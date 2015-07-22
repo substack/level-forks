8d06c126fd9be0ea79e8666a96ece64899915117
James Halliday <mail@substack.net>
2015-07-22 01:21:51 -0700

    passing chain test

---
+++ test/chain.js
@@ -0,0 +1,75 @@
+var test = require('tape');
+var collect = require('collect-stream');
+var cowfork = require('../');
+var memdb = require('memdb');
+var cow = cowfork(memdb())
+
+var chain = [
+  [
+    { type: 'put', key: 'a', value: 100 },
+    { type: 'put', key: 'b', value: 200 },
+    { type: 'put', key: 'c', value: 300 }
+  ],
+  [
+    { type: 'put', key: 'a', value: 123 },
+    { type: 'put', key: 'd', value: 400 },
+    { type: 'del', key: 'c' },
+  ],
+  [
+    { type: 'put', key: 'c', value: 333 }
+  ],
+  [
+    { type: 'put', key: 'e', value: 555 }
+  ]
+];
+
+var expected = [
+  [
+    { key: 'a', value: 100 },
+    { key: 'b', value: 200 },
+    { key: 'c', value: 300 },
+  ],
+  [
+    { key: 'a', value: 123 },
+    { key: 'b', value: 200 },
+    { key: 'd', value: 400 }
+  ],
+  [
+    { key: 'a', value: 123 },
+    { key: 'b', value: 200 },
+    { key: 'c', value: 333 },
+    { key: 'd', value: 400 }
+  ],
+  [
+    { key: 'a', value: 123 },
+    { key: 'b', value: 200 },
+    { key: 'c', value: 333 },
+    { key: 'd', value: 400 },
+    { key: 'e', value: 555 }
+  ]
+];
+
+test('chain', function (t) {
+  // populate with a linear chain of updates
+  var batches = chain.slice();
+  t.plan(expected.length * 2 + batches.length);
+  
+  ;(function next (seq, prev) {
+    if (batches.length === 0) return ready();
+    var c = cow.create(seq, prev, { valueEncoding: 'json' });
+    c.batch(batches.shift(), function (err) {
+      t.ifError(err, 'batch ' + seq);
+      next(seq + 1, seq)
+    });
+  })(0, null);
+
+  function ready () {
+    expected.forEach(function (ex, seq) {
+      var r = cow.open(seq, { valueEncoding: 'json' }).createReadStream();
+      collect(r, function (err, rows) {
+        t.ifError(err);
+        t.deepEqual(ex, rows, 'sequence ' + seq);
+      });
+    });
+  }
+});
